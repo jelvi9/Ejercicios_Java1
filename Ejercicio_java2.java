5e6345ac7f46ce3c21962662551efa7f382dd64c
jelvi9 <109859627+jelvi9@users.noreply.github.com>
2022-11-26 18:12:24 -0600

    Add files via upload

---
+++ Ejercicio_java2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicios_java;
+
+/**
+ *
+ * @author jelvi
+ */
+public class Ejercicios_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
